ea124b3fbd34863acd72be6d00fb50cc920add24
Micah Banschick <micha.ban@gmail.com>
2020-08-17 18:23:19 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  
+  attr_accessor :name, :weight, :height
+  
+  @@all = []
+  
+  def initialilze(params) 
+    @name = args[pirate][name]
+    @weight = args[pirate][weight]
+    @height = args[pirates][height]
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  
+  attr_accessor :name, :type, :booty
+  
+  @@all = []
+  
+  def initialilze(params) 
+    @name = args[ship][name]
+    @type = args[ship][type]
+    @booty = args[ship][booty]
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.clear
+    @@all = []
+  end 
+  
 end
